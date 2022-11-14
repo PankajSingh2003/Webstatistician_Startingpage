a82fdb00f12ed22728d792c35d9d6946e70f6fb0
Pankaj Singh <pankaj.2125csit1098@kiet.edu>
2022-11-14 19:53:58 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,40 @@
+import svgMap from 'svgmap';
+import 'svgmap/dist/svgMap.min.css';
+
+
+var limit = 1001;
+space = document.getElementById("space");
+
+stars = {
+  rand: function () {
+    return Math.random();
+  },
+
+  createStar: function () {
+    let star = document.createElement("div");
+    star.className = "star";
+
+    return star;
+  },
+
+  create: function () {
+    for (let i = 0; i <= limit; i++) {
+      let star = this.createStar();
+      star.style.top = `${this.rand() * 100}%`;
+      star.style.left = `${this.rand() * 100}%`;
+      star.style.animationDelay = `${this.rand() * 8}s`;
+      space.appendChild(star);
+    }
+  }
+};
+
+stars.create();
+
+function light_dark_mode() {
+  const body = document.body;
+  body.classList.toggle("light-mode");
+  const moon = document.querySelector('.bg3')  
+  moon.classList.toggle("moon_invert");
+  // if (document.getElementById("mode_button").innerHTML.trim() == "Light Mode") document.getElementById("mode_button").innerHTML = "Dark Mode";
+  // else document.getElementById("mode_button").innerHTML = "Light Mode";
+}
